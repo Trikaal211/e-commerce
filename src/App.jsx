3bd2f4631f68e1094ab070c5317878d2e407d435
Trikaal212 <anikets993@gmail.com>
2025-07-24 18:13:41 +0530

    add hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { HomePage } from './Pages/HomePage'
-import { BrowserRouter,Routes, Route} from 'react-router'
+import { HashRouter,Routes, Route} from 'react-router'
 import { CheckoutPage } from './Pages/CheckoutPage'
 import { OrdersPage } from './Pages/OrdersPage'
 
@@ -9,14 +9,14 @@ import './App.css'
 function App() {
 
   return (
-    <BrowserRouter basename="/e-commerce" >
+    <HashRouter basename="/e-commerce" >
     <Routes>
       <Route index element={ <HomePage/>} />
        <Route path="/checkout" element={<CheckoutPage/>} />
        <Route path="/orders" element={<OrdersPage/>} />
 
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
    
    
   )
