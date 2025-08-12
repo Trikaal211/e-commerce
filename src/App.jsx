f071726f5ca347c95692a9a13c819e9bdbd75c7a
Trikaal212 <anikets993@gmail.com>
2025-08-12 20:22:45 +0530

    add ecommerce backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { HomePage } from './Pages/HomePage'
-import { HashRouter,Routes, Route} from 'react-router-dom'
+import { Routes, Route} from 'react-router'
 import { CheckoutPage } from './Pages/CheckoutPage'
 import { OrdersPage } from './Pages/OrdersPage'
 
 import './App.css'
-
-
 function App() {
 
   return (
-    <HashRouter>
+ 
     <Routes>
       <Route index element={ <HomePage/>} />
        <Route path="/checkout" element={<CheckoutPage/>} />
        <Route path="/orders" element={<OrdersPage/>} />
 
       </Routes>
-      </HashRouter>
+  
    
    
   )
