df6a88c177af084990d6150cdfe84f6636c3c68a
Trikaal212 <anikets993@gmail.com>
2025-08-12 23:13:30 +0530

    lift cart up and share it bw pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { HomePage } from './Pages/HomePage'
+import axios from 'axios'
+import { useState,useEffect } from 'react'
 import { Routes, Route} from 'react-router'
 import { CheckoutPage } from './Pages/CheckoutPage'
 import { OrdersPage } from './Pages/OrdersPage'
 
 import './App.css'
 function App() {
+    const [cart, setCart] = useState([]);
+  useEffect(()=>{
+     axios.get('/api/cart-items')
+ .then((response)=>{
+  setCart(response.data);
+ })
+  },[]);
 
   return (
  
     <Routes>
-      <Route index element={ <HomePage/>} />
+      <Route index element={ <HomePage cart = {cart}/>} />
        <Route path="/checkout" element={<CheckoutPage/>} />
        <Route path="/orders" element={<OrdersPage/>} />
 
